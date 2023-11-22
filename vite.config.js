f39c1d76d263b79370ddf387e8c41f3942d1968f
Kateryna Verveda <katushka211@gmail.com>
2023-11-22 13:25:23 +0100

    add changes

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import { defineConfig } from "vite";
 export default defineConfig({
   base: "/weather-app",
   resolve: {
-    includePaths: ["node_modules"],
+    includePaths: ["node_modules", "./public"],
   },
 });
