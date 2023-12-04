3ce9141ec64ecbb16f394c67e5e20f38ef157ea6
Kateryna Verveda <katushka211@gmail.com>
2023-12-04 12:44:35 +0100

    update main.js

--- main.js
+++ main.js
@@ -1,1 +1,65 @@
+import { document } from "postcss";
 import "./style.css";
+import { log } from "console";
+
+const app = document.querySelector(".weather-app");
+const temp = document.querySelector(".temp");
+const dateOutput = document.querySelector(".date");
+const timeOutput = document.querySelector(".time");
+const conditionOutput = document.querySelector(".condition");
+const nameOutput = document.querySelector(".name");
+const icon = document.querySelector(".icon");
+const cloudOutput = document.querySelector(".cloud");
+const humidityOutput = document.querySelector(".humidity");
+const windOutput = document.querySelector(".wind");
+const form = document.getElementById("locationInput");
+const search = document.querySelector(".search");
+const btn = document.querySelector(".submit");
+const cities = document.querySelector(".city");
+
+let cityInput = "Kyiv";
+
+cities.forEach((city) => {
+  city.addEventListener("click", (e) => {
+    cityInput = e.target.innerHTML;
+    fetchWeatherData();
+    app.style.opacity = "0";
+  });
+});
+
+form.addEventListener("submit", (e) => {
+  if (search.value.length === 0) {
+    alert("Please, enter a city name");
+  } else {
+    cityInput = search.value;
+    fetchWeatherData();
+    search.value = "";
+    app.style.opacity = "0";
+  }
+  e.preventDefault();
+});
+
+const dayOfWeek = (day, month, year) => {
+  const weekday = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  return weekday[new Date(`${day}/${month}/${year}`).getDay()];
+};
+
+const fetchWeatherDate = () => {
+  fetch(
+    `https://www.weatherapi.com/v1/current.json?key=ec006f81799640bc83a113739230412=${cityInput}`
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      temp.innerHTML = data.current.temp_c + "&#176";
+      conditionOutput.innerHTML = data.current.condition.text;
+    });
+};
