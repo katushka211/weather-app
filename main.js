5bc5c24fbbc70475929b63ee69daadb2c279a385
Kateryna Verveda <katushka211@gmail.com>
2023-12-10 00:36:13 +0100

    add weather data

--- main.js
+++ main.js
@@ -1,6 +1,4 @@
-import { document } from "postcss";
 import "./style.css";
-import { log } from "console";
 
 const app = document.querySelector(".weather-app");
 const temp = document.querySelector(".temp");
@@ -15,9 +13,9 @@ const windOutput = document.querySelector(".wind");
 const form = document.getElementById("locationInput");
 const search = document.querySelector(".search");
 const btn = document.querySelector(".submit");
-const cities = document.querySelector(".city");
+const cities = document.querySelectorAll(".city");
 
-let cityInput = "Kyiv";
+let cityInput = "London";
 
 cities.forEach((city) => {
   city.addEventListener("click", (e) => {
@@ -28,7 +26,7 @@ cities.forEach((city) => {
 });
 
 form.addEventListener("submit", (e) => {
-  if (search.value.length === 0) {
+  if (search.value.length == 0) {
     alert("Please, enter a city name");
   } else {
     cityInput = search.value;
@@ -52,14 +50,111 @@ const dayOfWeek = (day, month, year) => {
   return weekday[new Date(`${day}/${month}/${year}`).getDay()];
 };
 
-const fetchWeatherDate = () => {
+const fetchWeatherData = () => {
   fetch(
-    `https://www.weatherapi.com/v1/current.json?key=ec006f81799640bc83a113739230412=${cityInput}`
+    `https://api.weatherapi.com/v1/current.json?key=f4709a4e27c0499d9a9160404230912&q=${cityInput}`
   )
     .then((response) => response.json())
     .then((data) => {
-      console.log(data);
       temp.innerHTML = data.current.temp_c + "&#176";
       conditionOutput.innerHTML = data.current.condition.text;
+
+      const date = data.location.localtime;
+      const y = parseInt(date.substr(0, 4));
+      const m = parseInt(date.substr(5, 2));
+      const d = parseInt(date.substr(8, 2));
+      const time = date.substr(11);
+
+      dateOutput.innerHTML = `${dayOfWeek(d, m, y)} ${d}, ${m}, ${y}`;
+
+      timeOutput.innerHTML = time;
+
+      nameOutput.innerHTML = data.location.name;
+
+      const iconId = data.current.condition.icon.substr(
+        "//cdn.weatherapi.com/64x64/".length
+      );
+      let timeOfDay = "day";
+      icon.src = `./icons/${timeOfDay}/${iconId}`;
+
+      cloudOutput.innerHTML = data.current.cloud + "%";
+      humidityOutput.innerHTML = data.current.humidity + "%";
+      windOutput.innerHTML = data.current.wind_kph + "km/h";
+
+      const code = data.current.condition.code;
+
+      if (!data.current.is_day) {
+        timeOfDay = "night";
+      }
+
+      if (code == 1000) {
+        app.style.backgroundImage = `url(./images/${timeOfDay}/clear.jpg)`;
+
+        btn.style.background = "#e5ba92";
+        if (timeOfDay == "night") {
+          btn.style.background = "#181e27";
+        }
+      } else if (
+        code == 1003 ||
+        code == 1006 ||
+        code == 1009 ||
+        code == 1030 ||
+        code == 1069 ||
+        code == 1087 ||
+        code == 1135 ||
+        code == 1273 ||
+        code == 1276 ||
+        code == 1279 ||
+        code == 1282
+      ) {
+        app.style.backgroundImage = `
+      url(./images/${timeOfDay}/cloudy.jpg)`;
+        btn.style.background = "#fa6d1b";
+        if (timeOfDay == "night") {
+          btn.style.background = "#181e27";
+        }
+      } else if (
+        code == 1063 ||
+        code == 1069 ||
+        code == 1072 ||
+        code == 1150 ||
+        code == 1153 ||
+        code == 1180 ||
+        code == 1183 ||
+        code == 1186 ||
+        code == 1189 ||
+        code == 1192 ||
+        code == 1195 ||
+        code == 1204 ||
+        code == 1207 ||
+        code == 1240 ||
+        code == 1243 ||
+        code == 1246 ||
+        code == 1249 ||
+        code == 1252
+      ) {
+        app.style.background = "#647d75";
+        if (timeOfDay == "night") {
+          btn.style.background = "#325c80";
+        }
+      } else {
+        app.style.backgroundImage = `url(.images/$(timeOfDay)/snowy.jpg)`;
+        btn.style.background = "#4d72aa";
+        if (timeOfDay == "night") {
+          btn.style.background = "#1b1b1b";
+        }
+      }
+      app.style.opacity = "1";
+    })
+    .catch((error) => {
+      console.error("Fetch error:", error);
     });
+  // .catch(() => {
+  //   alert("City not found, please try again");
+  //   app.style.opacity = "1";
+  // });
 };
+
+fetchWeatherData();
+
+app.style.opacity = "1";
